9c100589d38b466a3eaa5480bd8d97d21ad91352
Dimas Ardiminda <113449449+DimzsArdiminda@users.noreply.github.com>
2024-06-06 16:23:05 +0700

    wait comment from client

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,21 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nama = urlencode($_POST['name']);
+    $subjek = urlencode($_POST['subject']);
+    $message = urlencode($_POST['message']);
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+    // Nomor telepon WhatsApp tujuan
+    $phone_number = '087803479806';
+    
+    // Format pesan untuk dikirim via WhatsApp
+    $apiWA = "https://api.whatsapp.com/send?phone=$phone_number&text=Halo%20Admin,%20Saya%20$nama%20ingin%20bertanya%20mengenai%20$subjek.%20$message";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+    // Untuk debugging: cetak URL yang dihasilkan
+    echo "URL: " . htmlspecialchars($apiWA);
+    exit();
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+    // Redirect ke URL WhatsApp
+    // header("Location: $apiWA");
+    // exit();
+}
 ?>
